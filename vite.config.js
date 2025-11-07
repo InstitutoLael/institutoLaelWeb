7cec89893cc51d784a38b02b3b22a5019d236456
Diego Chaparro <contacto@institutolael.cl>
2025-11-07 02:10:10 -0300

    fix: rutas relativas para Cloudflare Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/institutoLaelWeb/",   // 👈 nombre EXACTO del repo
+  base: "./",               // ✅ rutas relativas para Cloudflare Pages
 });
